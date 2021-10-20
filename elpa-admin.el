7c525ccba1f7f380f46bcc364c78d4e6bf831652
Jonas Bernoulli <jonas@bernoul.li>
2021-10-20 02:49:43 +0200

    * elpa-admin.el (elpaa--pull): Don't update if upstream is missing
    After a new package has been added locally but before pushing it to
    "origin", the user may run "make build/<pkgname>" more than once.
    Starting with the second run this tried to merge from the respective
    branch on "origin" but since that doesn't exist yet, that failed.

--- elpa-admin.el
+++ elpa-admin.el
@@ -1528,11 +1528,13 @@ arbitrary code."
         (message "Running git pull in %S" default-directory)
         (elpaa--call t "git" "pull"))
        ((file-exists-p ".git")          ;A worktree, presumably.
-        (let ((status
-               (with-temp-buffer
-                 (let ((elpaa--debug nil))
-                   (elpaa--call t "git" "status" "--branch" "--porcelain=2"))
-                 (buffer-string))))
+        (let* ((status
+		(with-temp-buffer
+                  (let ((elpaa--debug nil))
+                    (elpaa--call t "git" "status" "--branch" "--porcelain=2"))
+                  (buffer-string)))
+	       (br (match-string 1 status))
+               (ortb (concat "refs/remotes/origin/" br)))
           (if (string-match (regexp-quote "\n# branch.ab +0 -0") status)
               (elpaa--message "%s up-to-date" dirname)
             ;; Set upstream if applicable.
@@ -1545,13 +1547,14 @@ arbitrary code."
                             (regexp-quote elpaa--branch-prefix)
                             ".*\\)")
                     status))
-              (let* ((br (match-string 1 status))
-                     (ortb (concat "refs/remotes/origin/" br)))
-                ;; There is an upstream to set it to!
-                (when (elpaa--git-branch-p ortb)
-                  (elpaa--call t "git" "branch" "--set-upstream-to" ortb))))
-            (message "Updating worktree in %S" default-directory)
-            (elpaa--call t "git" "merge"))))
+              ;; There is an upstream to set it to!
+              (when (elpaa--git-branch-p ortb)
+                (elpaa--call t "git" "branch" "--set-upstream-to" ortb)))
+	    (if (elpaa--git-branch-p ortb)
+		(progn
+		  (message "Updating worktree in %S" default-directory)
+		  (elpaa--call t "git" "merge"))
+	      (message "Not pushed to origin yet.  Not updating worktree")))))
        (t (error "No .git in %S" default-directory)))
       (unless (and (eobp) (bobp))
         (message "Updated %s:%s%s" dirname
